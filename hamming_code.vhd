0f2b23a8e5817e7e8f4b346bb4c8409366912b66
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2021-12-20 18:51:05 +0200

    Add files via upload

---
+++ hamming_code.vhd
@@ -0,0 +1,26 @@
+module hamming_code
+(    input serial_in,
+     input clk,
+	  input write,
+     output serial_out
+);
+wire en_decoder;
+wire [6:0] data1;
+encoder c1
+(     .clk(clk),
+	   .data_in(serial_in),
+	   .write(write),
+	   .ready(en_decoder),
+	   .data_out(data1)
+);
+decoder c2
+(     .in(data1),
+      .clk(clk), 
+      .enable(en_decoder),
+	   .serial_out(serial_out)
+);
+
+endmodule
+
+
+
